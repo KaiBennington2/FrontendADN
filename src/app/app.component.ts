fd826965cf01a2b8c9ce73b1348f6b2535bbe3fe
Jhonatan Stevan Parra Atehortua - Ceiba Software <stevan.parra@ceiba.com.co>
2022-04-26 12:54:12 -0500

    2. Se agrega el tipo de procesador de estilos (.SCSS) al proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { MenuItem } from '@core/modelo/menu-item';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app-base';
